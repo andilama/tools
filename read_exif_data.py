cac10ce2bbdea5bf412637f5dfded80d406c3e2c
Andreas Lahrmann <andilahrmann@gmail.com>
2015-12-30 22:59:51 +0100

    Expanded use of Image tags

--- read_exif_data.py
+++ read_exif_data.py
@@ -1,12 +1,33 @@
-import os
 import exifread
 from datetime import datetime
 
-f = open(r"C:\Users\Julia\Pictures\20140106\DSC05572.JPG", 'rb')
+f = open(r"/home/andreas/Bilder/Lago Maggiore/P1110643.JPG", 'rb')
 
-tags = exifread.process_file(f)
+#process  file with exifread without thumbnails
+tags = exifread.process_file(f, details=False)
 
-exif_date = str(tags['EXIF DateTimeDigitized'])
+#relevant tags for processing
+checktags = ['Image Make', 'Image Model', 'EXIF DateTimeDigitized',
+             'EXIF DateTimeOriginal']
+fileinfo = {}         
 
+#datetime format
 dt_format = "%Y:%m:%d %H:%M:%S"
-exif_dt = datetime(exif_date, dt_format)
+try:
+        for tag in checktags:
+            fileinfo[tag] = tags[tag]
+            
+        exif_date = str(tags['EXIF DateTimeDigitized'])
+        exif_dt = datetime.strptime(exif_date, dt_format)
+        fileinfo['EXIF DateTimeDigitized'] = exif_dt
+        
+        exif_date = str(tags['EXIF DateTimeOriginal'])
+        exif_dt = datetime.strptime(exif_date, dt_format)
+        fileinfo['EXIF DateTimeOriginal'] = exif_dt
+except KeyError:
+    print("key not found")
+except ValueError:
+    print("Format not found")
+
+for k,v in fileinfo.items():
+    print(k,v)
